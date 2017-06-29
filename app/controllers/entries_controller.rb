5ff9457ee3d2f1c636da8d13c47c608543d2e5f7
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-06-29 14:53:15 -0400

    Worked on style of blog posts

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,26 @@
 class EntriesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def show
-    @entry = Entry.where(directory: params[:directory], slug: params[:slug]).first
+    @entry = Entry.where(slug: params[:slug]).first
   end
+
+  def edit
+    @entry = Entry.where(slug: params[:slug]).first
+  end
+
+  def update
+    @entry = Entry.where(slug: params[:slug]).first
+
+    if @entry.update_attributes(entry_params)
+      flash.notice = ""
+    end
+  end
+
+  private
+
+    def entry_params
+      params.require(:blog).permit(:title, :body, :slug)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root "records#index"
 
   get 'says/:slug', to: 'entries#show'
+  get 'says/:slug/edit', to: 'entries#edit'
+  put 'says/:slug', to: 'entries#update'
+  patch 'says/:slug', to: 'entries#update'
 
   # get ':directory/:slug', to: 'entries#show', constraints: lambda { |request|
   #   Entry::DIRECTORIES.include? request.path_parameters['directory']
