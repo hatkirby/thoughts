c2fe86394bac998d887c5f59b48849dc067b9e43
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-10-15 14:18:07 -0400

    Fixed MySQL choking on a pokeviewer migration
    See hatkirby/pokeviewer@a29b86011d9c53709279d125f4501140c17ccc1e.

--- db/migrate/20171015171554_rename_revision_hold_item.pokeviewer.rb
+++ db/migrate/20171015171554_rename_revision_hold_item.pokeviewer.rb
@@ -1,8 +1,10 @@
 # This migration comes from pokeviewer (originally 20170930213633)
 class RenameRevisionHoldItem < ActiveRecord::Migration[5.1]
   def change
+    remove_column :pokeviewer_revisions, :hold_item, :integer
+
     change_table :pokeviewer_revisions do |t|
-      t.rename :hold_item, :item_id
+      t.integer :item_id
     end
 
     add_foreign_key :pokeviewer_revisions, :pokeviewer_items, column: :item_id
