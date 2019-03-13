837537b14b01a8c7504b786e1bbf7a350c21242a
Kelly Rauchenberger <fefferburbia@gmail.com>
2019-03-12 22:43:12 -0400

    Game by status filter technically exists

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,9 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
+
+    if params[:status]
+      @games = @games.where(status: params[:status])
+    end
   end
 end
