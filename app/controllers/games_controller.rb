b62d43ccde70aa6fccf5341e57a695a2cfb289c1
Kelly Rauchenberger <fefferburbia@gmail.com>
2019-03-13 15:48:14 -0400

    Game list is sortable

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,21 @@
 class GamesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @games = Game.order(started_on: :desc)
+    @games = Game.order(sort_column + " " + sort_direction)
 
     if params[:status]
       @games = @games.where(status: params[:status])
     end
   end
+
+  private
+
+    def sort_column
+      Game.column_names.include?(params[:sort]) ? params[:sort] : "started_on"
+    end
+
+    def sort_direction
+      %[asc desc].include?(params[:dir]) ? params[:dir] : "asc"
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     content_for :title, text
   end
 
+  def sortable(col, title = nil)
+    title ||= col.titleize
+    css_class = (col == sort_column) ? "current #{sort_direction}" : nil
+    direction = (col == sort_column and sort_direction == "asc") ? "desc" : "asc"
+    link_to title, {:sort => col, :dir => direction}, {:class => css_class}
+  end
+
 end
